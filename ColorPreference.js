4c4231e851200dce031607e36881601a54642f2b
guiannemarie <guiannemarie9@gmail.com>
2025-03-21 21:26:28 +0800

    ColorPreference.js

---
+++ ColorPreference.js
@@ -0,0 +1,8 @@
+let faveColors = [];
+let numberOfFaveColors = 5;
+
+for (let i = 0; i < numberOfFavColors; i++) {
+  let newColor = prompt("Enter a color you like:");
+  favColor.push(newColor);
+  console.log("Updated color list:", favColors;
+}
