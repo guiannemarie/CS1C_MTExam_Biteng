7ade9912a276a601e3c3cbfb02bdc7077503cd3d
guiannemarie <guiannemarie9@gmail.com>
2025-03-21 21:08:25 +0800

    UserAgeCategorization.js

---
+++ UserAgeCategorization.js
@@ -0,0 +1,16 @@
+let age = prompt("Please enter your age:")
+
+if (age <= 5){
+  console.log("You are a toddler.");
+}
+if age >=13-19){
+  console.log("You are a teenager");
+}
+if (age >=20-35){
+  console.log("You are a young adult");
+}
+if (age >=36-60){
+  console.log("You are a middle-aged");
+} else {
+  console.log("You are a senior")
+}
